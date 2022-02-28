93fdbc2bbdef957abccd10fb2cb3da6f5c35f013
daniialovexceedteam <daniyalbek.daniyalov@gmail.com>
2022-02-28 14:57:10 +0300

    refactor

--- migrations/env.py
+++ migrations/env.py
@@ -1,36 +1,20 @@
 import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy import engine_from_config
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 from alembic import context
 
+from user_app.models import *
 from quiz_project import Base
 from quiz_project.conf import DATABASE_URL
 
-from user_app.models import *
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
-
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline():
