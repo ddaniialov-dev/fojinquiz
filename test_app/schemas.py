193343c8d26209b61fb15604a6b5079031e9979a
mDanone <daniyalbek.daniyalov@gmail.com>
2022-03-15 15:01:12 +0300

    added update test schema

--- test_app/schemas.py
+++ test_app/schemas.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class TestSchema(BaseModel):
-    holder: str | None
-    published: bool
+    holder: int = None
+    published: bool = True
+
+
+class UpdateTestSchema(BaseModel):
+    title: str | None
+    published: bool = True
+    holder: int | None
+    published_date: datetime | None
