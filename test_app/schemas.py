bd56712e4fd15720b5f48a3cd21aef64fc5558e6
mDanone <daniyalbek.daniyalov@gmail.com>
2022-03-15 15:01:12 +0300

    added update test schema

--- test_app/schemas.py
+++ test_app/schemas.py
@@ -1,6 +1,17 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
 from quiz_project.behaviours import BaseSchema
 
 
 class TestSchema(BaseSchema):
     holder: int = None
     published: bool = True
+
+
+class UpdateTestSchema(BaseModel):
+    title: Optional[str]
+    published: bool = True
+    holder: Optional[int]
+    published_date: Optional[datetime]
+    
