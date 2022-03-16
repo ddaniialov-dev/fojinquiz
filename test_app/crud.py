4769a7cb80acb93df852c72ed68357cc1bdde137
mDanone <daniyalbek.daniyalov@gmail.com>
2022-03-16 13:30:22 +0300

    refactor crud

--- test_app/crud.py
+++ test_app/crud.py
@@ -1,43 +1,61 @@
-from sqlalchemy import delete, select, update
-from sqlalchemy import and_
+from typing import List
+
+from sqlalchemy import delete, select, update, and_
+
 from quiz_project import AbstractBaseManager
 
 from test_app.models import Test
-from test_app.schemas import TestSchema
+from test_app.schemas import TestCreate, TestUpdate
+
+from user_app.models import User
 
 
 class TestManager(AbstractBaseManager):
-    
-    async def get_tests(self, holder):
-        query = select(Test).where(Test.holder == holder.id)
+    async def get_tests(self) -> List[Test]:
+        query = select(Test)
         result = await self._database_session.execute(query)
-        return result.all()
+        return [result[0] for result in result.all()]
     
     async def get_user_tests(self, user_id: int) -> List[Test]:
-        query = select(Test).where(Test.holder == user_id)
+        query = (
+            select(Test)
+            .where(Test.holder == user_id)
+        )
+
         result = await self._database_session.execute(query)
-        return result.all()
+        return [result[0] for result in result.all()]
     
-    async def get_test(self, test_id: int):
-        query = select(Test).where(Test.id == test_id)
+    async def get_test(self, test_id: int) -> Test:
+        query = (
+            select(Test)
+            .where(Test.id == test_id)
+        )
+
         result = await self._database_session.execute(query)
         return result.first()
 
-    async def create_test(self, test: TestSchema) -> int:
+    async def create_test(self, test: TestCreate) -> Test:
         test_object = Test(holder=test.holder, published=test.published)
         await self.create(test_object)
-        return test_object.id
+        return test_object
 
-    async def delete_test(self, holder, test_id: int):
-        query = delete(Test).where(
-            and_(Test.id == test_id, holder.id == Test.holder)
-            )
-        return await self._database_session.execute(query)
-    
-    async def update_test(self, holder, test_id: int, data):
-        query =  update(Test).returning(Test).where(
-            and_(Test.id==test_id, Test.holder == holder.id)
-            ).values(**data)
-        result =  await self._database_session.execute(query)
+    async def delete_test(self, holder: User, test_id: int) -> None:
+        query = (
+            delete(Test)
+            .returning(Test)
+            .where(and_(Test.id == test_id, holder.id == Test.holder))
+        )
+
+        result = await self._database_session.execute(query)
         return result.first()
     
+    async def update_test(self, holder: User, test_id: int, data: dict) -> Test:
+        query =  (
+            update(Test)
+            .returning(Test)
+            .where(and_(Test.id==test_id, Test.holder == holder.id))
+            .values(**data)
+        )
+
+        result =  await self._database_session.execute(query)
+        return result.first()
