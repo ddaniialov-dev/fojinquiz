5ad1c5825fa91e5cd8e22abbf69f024a6c37149a
mDanone <daniyalbek.daniyalov@gmail.com>
2022-03-16 13:31:08 +0300

    added view for all tests and handled not found

--- test_app/views.py
+++ test_app/views.py
@@ -1,16 +1,14 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
 from fastapi_jwt_auth import AuthJWT
-
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, Depends, HTTPException, Response
 
-from test_app.schemas import TestSchema, UpdateTestSchema
-
-from quiz_project import JwtAccessRequired, get_session, token_header
-from user_app import UserManager
 from .crud import TestManager
-from quiz_project import get_current_user
+from quiz_project import (JwtAccessRequired, get_session,
+                          token_header, get_current_user)
+from test_app.schemas import TestGet, TestCreate, TestUpdate
+
 
 router = APIRouter(
     prefix='/tests',
@@ -22,16 +20,37 @@ router = APIRouter(
 @router.get(
     '/',
     status_code=200,
-    response_model=List[TestSchema]
+    response_model=List[TestGet]
 )
 @JwtAccessRequired()
-async def get_tests(
+async def get_user_tests(
     auth: AuthJWT = Depends(),
     database_session: AsyncSession = Depends(get_session)
-) -> List[TestSchema]:
+) -> List[TestGet]:
     user = await get_current_user(database_session, auth)
     async with TestManager(database_session) as test_manager:
-        tests = await test_manager.get_tests(user)
+        tests = await test_manager.get_user_tests(user.id)
+
+        if not tests:
+            raise HTTPException(
+                status_code=404, detail='data not found'
+            )
+
+    return tests
+
+
+@router.get(
+    '/all/',
+    status_code=200,
+    response_model=List[TestGet]
+)
+@JwtAccessRequired()
+async def get_all_tests(
+    auth: AuthJWT = Depends(),
+    database_session: AsyncSession = Depends(get_session)
+) -> List[TestGet]:
+    async with TestManager(database_session) as test_manager:
+        tests = await test_manager.get_tests()
 
         if not tests:
             raise HTTPException(
@@ -44,30 +63,28 @@ async def get_tests(
 @router.post(
     '/',
     status_code=201,
-    response_model=TestSchema
+    response_model=TestGet
 )
 @JwtAccessRequired()
 async def create_test(
-    test: TestSchema,
+    test: TestCreate,
     auth: AuthJWT = Depends(),
     database_session: AsyncSession = Depends(get_session)
 ) -> int:
-    user = get_current_user(database_session, auth)
+    user = await get_current_user(database_session, auth)
     async with TestManager(database_session) as test_manager:
-        test.holder = user[0].id
-        record_id = await test_manager.create_test(test)
-
-        if not record_id:
+        test.holder = user.id
+        result = await test_manager.create_test(test)
+        if not result:
             raise HTTPException(
                 status_code=400, detail='test wat not created'
             )
-
-    return record_id
+    return result
 
 
 @router.delete(
     '/{test_id}/',
-    status_code=204
+    status_code=204,
 )
 @JwtAccessRequired()
 async def delete_test(
@@ -75,9 +92,15 @@ async def delete_test(
     auth: AuthJWT = Depends(),
     database_session: AsyncSession = Depends(get_session)
 ) -> None:
-    user = get_current_user(database_session, auth)
+    user = await get_current_user(database_session, auth)
     async with TestManager(database_session) as test_manager:
-        await test_manager.delete_test(user, test_id)
+        result = await test_manager.delete_test(user, test_id)
+
+    if not result:
+        raise HTTPException(status_code=404, detail="data not found")
+
+    return Response()
+
 
 @router.get(
     '/{test_id}/',
@@ -89,7 +112,6 @@ async def get_test(
    auth: AuthJWT = Depends(),
    database_session: AsyncSession = Depends(get_session)
 ):
-    user = await get_current_user(database_session, auth)
     async with TestManager(database_session) as test_manager:
         result =  await test_manager.get_test(test_id)
     
@@ -102,12 +124,12 @@ async def get_test(
 
 @router.put(
     '/{test_id}/',
-    response_model=UpdateTestSchema,
+    response_model=TestUpdate,
     status_code=200
 )
 async def update_test(
     test_id: int,
-    test: UpdateTestSchema,
+    test: TestUpdate,
     auth: AuthJWT = Depends(),
     database_session: AsyncSession = Depends(get_session)
 ):
@@ -115,4 +137,9 @@ async def update_test(
     test.holder = user.id
     async with TestManager(database_session) as test_manager:
         result =  await test_manager.update_test(user, test_id, test.dict())
-    return result 
+    
+    if not result:
+        raise HTTPException(
+            status_code=404, detail="data not found"
+        )
+    return result
