376800bf01e744f4ff8c3591349b2880335061a7
mDanone <daniyalbek.daniyalov@gmail.com>
2022-03-18 11:50:40 +0300

    updated dependency for user

--- quiz_project/utils/dependencies.py
+++ quiz_project/utils/dependencies.py
@@ -17,8 +17,9 @@ def token_header(authorization: str = Header(...)):
 
 async def get_current_user(
     db_session: AsyncSession = Depends(get_session),
-    auth: AuthJWT = Depends()
+    auth: AuthJWT = Depends(token_header)
 ):
+    auth.jwt_required()
     async with user_app.crud.UserManager(db_session) as user_manager:
         user = await user_manager.get_user_by_username(auth.get_jwt_subject())
     return user[0]
