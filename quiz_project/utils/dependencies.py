af3ec3bbfa21df2a49529c1389c56d930742b678
daniialovexceedteam <ddaniialov.exceedteam@gmail.com>
2022-03-21 15:17:51 +0300

    repaired user auth

--- quiz_project/utils/dependencies.py
+++ quiz_project/utils/dependencies.py
@@ -1,6 +1,9 @@
-from fastapi_jwt_auth import AuthJWT
+from fastapi import Header, HTTPException, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Header, HTTPException, Depends
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import JWTDecodeError
+
+from user_app import UserManager
 
 from quiz_project.database import get_session
 from user_app.crud import UserManager
@@ -19,8 +22,16 @@ async def get_current_user(
     token_auth = Depends(token_header),
     db_session: AsyncSession = Depends(get_session)
 ):
-    auth.jwt_required()
-    async with user_app.UserManager(db_session) as user_manager:
+    try:
+        auth.jwt_required()
+    except JWTDecodeError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    async with UserManager(db_session) as user_manager:
         user = await user_manager.get_user_by_username(auth.get_jwt_subject())
-
-    return user[0]
+    if user:
+        user = user[0]
+    return user
