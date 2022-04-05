ebea409bbd952d5099a63e0e4244c01fda2c3a30
ddaniialov <ddaniialov.exceedteam@gmail.com>
2022-04-05 15:54:23 +0300

    added answer views

--- test_app/views/answer_views.py
+++ test_app/views/answer_views.py
@@ -15,7 +15,7 @@ from user_app.models import User
 
 
 answer_router = APIRouter(
-    prefix='/answers',
+    prefix='/questions/{question_id}',
     tags=['answers'],
 )
 
@@ -26,11 +26,114 @@ answer_router = APIRouter(
     response_model=GetAnswer
 )
 async def create_answer(
+    question_id: int,
     answer: CreateAnswer,
     auth: User = Depends(get_current_user),
     database_session: AsyncSession = Depends(get_session)
 ):
     async with AnswerManager(database_session) as answer_manager:
-        answer_object = await answer_manager.create_answer(auth, answer)
+        answer_object = await answer_manager.create_answer(auth, question_id, answer)
 
     return answer_object
+
+
+@answer_router.get(
+    '/answers/',
+    status_code=200,
+    response_model=List[GetAnswer] 
+)
+async def get_answers(
+    question_id: int,
+    auth: User = Depends(get_current_user),
+    database_session: AsyncSession = Depends(get_session)
+):
+    async with AnswerManager(database_session) as answer_manager:
+        answer_objects = await answer_manager.get_answers(auth, question_id)
+
+        if not answer_objects:
+            raise HTTPException(
+                status_code=404, detail='data not found'
+            )
+    return answer_objects
+
+
+@answer_router.get(
+    '/answers/{answer_id}',
+    status_code=200,
+    response_model=GetAnswer
+)
+async def get_answers(
+    question_id: int,
+    auth: User = Depends(get_current_user),
+    database_session: AsyncSession = Depends(get_session)
+):
+    async with AnswerManager(database_session) as answer_manager:
+        answer_object = await answer_manager.get_answers(auth, question_id)
+
+        if not answer_object:
+            raise HTTPException(
+                status_code=404, detail='data not found'
+            )
+    return answer_object
+
+@answer_router.get(
+    '/answers/{answer_id}/',
+    status_code=200,
+    response_model=GetAnswer
+)
+async def get_answer(
+    question_id: int,
+    auth: User = Depends(get_current_user),
+    database_session: AsyncSession = Depends(get_session)
+):
+    async with AnswerManager(database_session) as answer_manager:
+        answer_object = await answer_manager.get_answers(auth, question_id)
+
+        if not answer_object:
+            raise HTTPException(
+                status_code=404, detail='data not found'
+            )
+    return answer_object
+
+
+@answer_router.put(
+    '/answers/{answer_id}/',
+    status_code=200,
+    response_model=GetAnswer
+)
+async def update_answer(
+    question_id: int,
+    answer_id: int,
+    auth: User = Depends(get_current_user),
+    database_session: AsyncSession = Depends(get_session)
+):
+    async with AnswerManager(database_session) as answer_manager:
+        answer_object = await answer_manager.update_answer(auth, question_id, answer_id)
+
+        if not answer_object:
+            raise HTTPException(
+                status_code=404, detail='data not found'
+            )
+
+    return answer_object
+
+
+@answer_router.delete(
+    '/answers/{answer_id}/',
+    status_code=200,
+)
+async def delete_answer(
+    question_id: int,
+    answer_id: int,
+    auth: User = Depends(get_current_user),
+    database_session: AsyncSession = Depends(get_session)
+):
+    async with AnswerManager(database_session) as answer_manager:
+        answer_object = await answer_manager.delete_answer(auth, question_id, answer_id)
+
+        if not answer_object:
+            raise HTTPException(
+                status_code=404, detail='data not found'
+            )
+
+    return Response(status_code=204)
