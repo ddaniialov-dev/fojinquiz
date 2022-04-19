b51c135ad40b1a9ba8bc98bb0565bc825bb44fa9
solo <dsogomonian@gmail.com>
2022-04-19 18:32:31 +0300

    added checker file for questions, added type hints for checkers, added quetions file to init

--- test_app/checks/__init__.py
+++ test_app/checks/__init__.py
@@ -1,1 +1,10 @@
 from .common import check_if_exist, check_if_exists, check_if_holder
+from .questions import check_if_test_has_question
+
+
+__all__ = [
+  "check_if_exist",
+  "check_if_exists",
+  "check_if_holder",
+  "check_if_test_has_question"
+]
