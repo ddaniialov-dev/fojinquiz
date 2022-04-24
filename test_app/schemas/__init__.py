d16d963c6ded7ba74bb6983a7f9336194f60bcdf
ddaniialov <ddaniialov.exceedteam@gmail.com>
2022-04-24 09:56:04 +0300

    added schemas and routers to init files

--- test_app/schemas/__init__.py
+++ test_app/schemas/__init__.py
@@ -1,7 +1,7 @@
 from .test_schemas import GetTest, CreateTest, UpdateTest
 from .question_schemas import ImageSchema, GetQuestion, CreateQuestion, UpdateQuestion
 from .session_schemas import GetSession
-
+from .user_answer_schemas import GetUserAnswer, CreateUserAnswer
 
 __all__ = [
     "GetTest",
@@ -12,4 +12,6 @@ __all__ = [
     "CreateQuestion",
     "UpdateQuestion",
     "GetSession",
+    "GetUserAnswer",
+    "CreateUserAnswer"
 ]
--- test_app/views/__init__.py
+++ test_app/views/__init__.py
@@ -2,9 +2,12 @@ from .question_views import question_router
 from .test_views import test_router
 from .session_views import session_router
 from .answer_views import answer_router
+from .user_answer_views import user_answer_router
 
 __all__ = [
     "question_router",
     "test_router",
-    "session_router"
+    "session_router",
+    "answer_router",
+    "user_answer_router",
 ]
