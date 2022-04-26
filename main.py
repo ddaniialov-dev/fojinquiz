7a5730f9ddb31780386953c213ecf2cd5780e5bc
ddaniialov <ddaniialov.exceedteam@gmail.com>
2022-04-26 15:36:37 +0300

    added cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from user_app.views import user_router
 from test_app.views import (
@@ -11,6 +12,18 @@ from test_app.views import (
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router)
 app.include_router(question_router)
