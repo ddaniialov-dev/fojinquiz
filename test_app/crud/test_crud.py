c2d59eb3cb9ea9181c98a109f3f9c45906e11f31
solo <dsogomonian@gmail.com>
2022-04-27 14:51:06 +0300

    added ordering to questions and questions to test

--- test_app/crud/test_crud.py
+++ test_app/crud/test_crud.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from sqlalchemy import delete, select, update, and_
+from sqlalchemy.orm import selectinload
 
 from user_app.models import User
 from test_app.models import Test
@@ -27,6 +28,7 @@ class TestManager(AbstractBaseManager):
     async def get_test(self, test_id: int) -> Test:
         query = (
             select(Test)
+            .options(selectinload(Test.questions))
             .where(Test.id == test_id)
         )
 
