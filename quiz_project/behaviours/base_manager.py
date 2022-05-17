fbefcbb2570dedf0253eb7490cf3f7bac06f4fcd
ddaniialov <ddaniialov.exceedteam@gmail.com>
2022-05-17 16:24:50 +0300

    deleted

--- quiz_project/behaviours/base_manager.py
+++ quiz_project/behaviours/base_manager.py
@@ -17,12 +17,6 @@ class AbstractBaseManager:
         else:
             await self._database_session.rollback()
 
-    async def _before_create(self, *args, **kwargs):
-        pass
-
-    async def _after_create(self, *args, **kwargs):
-        pass
-
     async def create(self, obj):
         await self._before_create()
 
@@ -30,14 +24,3 @@ class AbstractBaseManager:
         await self._database_session.commit()
         await self._database_session.refresh(obj)
         await self._after_create()
-
-    async def _before_update(self, *args, **kwargs):
-        pass
-
-    async def _after_update(self, *args, **kwargs):
-        pass
-
-    async def update(self, *args, **kwargs):
-        await self._before_update(*args, **kwargs)
-        await self._database_session.commit()
-        await self._after_update(*args, **kwargs)
