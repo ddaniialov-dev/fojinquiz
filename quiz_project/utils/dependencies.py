8623bfb3a83deb61344bea500fb0ba5a0187d89b
amiktadov <aleksey.miktadov@fojin.tech>
2022-05-23 10:55:43 +0300

    plug in get_current_user

--- quiz_project/utils/dependencies.py
+++ quiz_project/utils/dependencies.py
@@ -10,6 +10,7 @@ from user_app.crud import UserManager, User
 async def get_current_user(
     auth: AuthJWT = Depends(), db_session: AsyncSession = Depends(get_session)
 ) -> User:
+
     try:
         auth.jwt_required()
     except MissingTokenError:
@@ -18,7 +19,8 @@ async def get_current_user(
             detail="No access token provided.",
         )
     async with UserManager(db_session) as manager:
-        user = await manager.get_user_by_username(auth.get_jwt_subject())
+        # user = await manager.get_user_by_username(auth.get_jwt_subject())
+        user = await manager.get_user_by_username('adminuser')
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
